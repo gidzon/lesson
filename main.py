1811bb26c9e1dfb130fbc0913a653083446a42a2
Vorobev Nicolay <sumakaki37rus@gmail.com>
2022-01-22 20:40:22 +0300

    include core module and refactor code

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-import requests
-import os.path
-from bs4 import BeautifulSoup
+import core
 
-filePath = 'php-net.html'
-if not os.path.isfile(filePath):
-    url = 'https://www.php.net/'
-    response = requests.get(url)
-    html = response.text
-    file = open('php-net.html', 'w')
-    file.write(html)
-    file.close()
-
-resultContent = []
-
-file = open('php-net.html', 'r')
-html = file.read()
-file.close()
-soup = BeautifulSoup(html, 'html.parser')
-articles = soup.findAll('article', class_='newsentry')
-
-
-for item in articles:
-    title = item.find('a').string
-    link = item.find('a').get('href')
-    content = item.find('div', class_='newscontent').div.p.string
-    resultContent.append({
-        'title' : title,
-        'link' : link,
-        'content' : content,
-    })
-
+response = core.webPageRequest('https://www.php.net/')
+contents = core.readPageToFile('php-net.html')
+listResult = core.parseContent([], contents)
